c3548e8d2d1eec853e9b209bc86c8836c7f2f31e
psravi <60989082+psravi@users.noreply.github.com>
2023-03-26 21:21:04 -0400

    json-raw removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
+#streamlit.text(fruityvice_response.json())
 # take the json of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as a table
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)  # output as a table
