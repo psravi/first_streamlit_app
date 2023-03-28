14995dc357de04258bd34a2e1eeef418cbf364db
psravi <60989082+psravi@users.noreply.github.com>
2023-03-27 21:07:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice):
-    try 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        return fruityvice_normalized
-    except as e:
-        streamlit.error()
+def get_fruityvice_data(this_fruit_choice):    
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
     
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
